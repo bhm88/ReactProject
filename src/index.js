9173e676ad37f4c842b7efb700200acdf7d054ba
Bharat.Hiremath <bharathphm@yahoo.in>
2020-05-27 23:27:51 +0530

    firstTopic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+//let city = "madrid";
+
+let city = {
+  name: "madrid",
+  country: "Spain",
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  //createelement takes 3 arguments,1st is h1 tag/div,2nd is argument to 1st element like
+  //properties and third one is children
+  //React.createElement("h1", { style: { color: "green" } }, "Hello !"),
+  //React.createElement(
+  //   "div",
+  //   { style: { color: "red" } },
+  //   React.createElement("h1", null, "hi again inside parent")
+  // ),
+  //lists where jsx comes into picture,babel allow us to use jsx syntax and converts it into browser compatible
+  //code ,internally its like creating React.createElement("li",null,"ammi")
+  //jsx is extension to xml and simillar to html
+  // <h1>hello from {city} !</h1>,
+  // <ul>
+  //   <li>ammi</li>
+  //   <li>gummi</li>
+  // </ul>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <h1 id="heading" className="cool-text">
+    {city.name} is in {city.country}
+  </h1>,
+  document.getElementById("root")
+);
